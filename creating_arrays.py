8d9a430a75e2e176280024071f2aaeed82139c36
Jerome Chua <144978697+chuajeromepython@users.noreply.github.com>
2024-06-06 13:47:48 +0800

    Update and rename np_creating_arrays.py to creating_arrays.py

--- creating_arrays.py
+++ creating_arrays.py
@@ -1,18 +1,16 @@
 import numpy as np
 
-print('='*(130-17))
-print('--> using np.array\n')
-
-a = np.array([1,2,3],dtype=np.int32) # can be initialized with a generic list
-b = np.array((1,2,3),dtype=np.int32) # can be initialized with a generic tuple
+# several ways to create numpy arrays
+a = np.array([1,2,3],dtype=np.int32) # can be initialized with a python list
+b = np.array((1,2,3),dtype=np.int32) # can be initialized with a python tuple
 c = np.array([i for i in range(10)],dtype=np.int32) # can be initialized with a list comprehension
 d = np.array(tuple(i for i in range(10)),dtype=np.int32) # can be initialized with a tuple comprehension
 e = np.array(list(dict(a=1,b=2,c=3).values()),dtype=np.int32) # can be initialized with the values of a dictionary
 f = np.array(list({'a':1,'b':2,'c':3}.values()),dtype=np.int32) # same as the above
-g = np.array('hello,world,python,spam'.split(','),dtype='<U5') # with strings using split
-h = np.array(['hello','world','python','spam'],dtype='<U5') # normal list of strings
+g = np.array('hello,world,python,spam'.split(','),dtype='<U5') # with strings using split (not recommended)
+h = np.array(['hello','world','python','spam'],dtype='<U5') # normal list of strings (not recommended)
 j = np.array('hello world') # normal string
-k = np.linspace(0,10,num=100) # linspace creates an array of some type staring with a start and ending with an end, num specifies the total amount of elements
+k = np.linspace(0,10,num=100) # linspace creates an array staring with a start (m) and ending with (n-1), parameter "num" specifies the total amount of elements in the array
 l = np.array(k) # or using other ndarray objects
 
 print(a)
@@ -27,20 +25,21 @@ print(j)
 print(k)
 print(l)
 
+"""
 m = np.array([1,2,3,4])
 print(m)
 print(m.ndim, np.ndim(m))
 print(m.shape, np.shape(m))
 print(m.size, np.size(m))
+"""
 
-n = np.array([[1,2],[3,4]]) # create a two dimensional array 2 x 2
+n = np.array([[1,2],[3,4]]) # create a two dimensional array (2 x 2)
 print(n)
-print(n.ndim, np.ndim(n))
-print(n.shape, np.shape(n))
-print(n.size, np.size(n))
+print(n.ndim, np.ndim(n)) # 2
+print(n.shape, np.shape(n)) # (2,2)
+print(n.size, np.size(n)) # 4
 
-print('='*(130-17))
-print('--> arrays filled with constant values 1s and 0s\n')
+# creating arrays filled with constant values 1s and 0s
 
 o = np.zeros(10) # create a flat array of 0 elements the size of 10
 print(o)
@@ -61,7 +60,9 @@ t = np.ones((5,3,5),dtype=np.int32)
 print(t)
 
 u = np.ones(10)
-print(u * 5.4, 5.4 * u) # order doesn't matter
+print(u * 5.4, 5.4 * u) # u is multiplied by the scalar 5.4
+# results in a 1d array the size of 10 filled with 5.4 values
+# multiplication order doesn't matter
 
 #or 
 
@@ -77,6 +78,6 @@ x1.fill(9.5) # fill() replaces all the values in the array with the specified va
 print(x1)
 
 x2 = np.array([[1,2,3],[2,3,4],[3,4,5]])
-x2.fill(1.1)
+x2.fill(1.1) # fills the entire array with 1.1 values
 
 print(x2)
